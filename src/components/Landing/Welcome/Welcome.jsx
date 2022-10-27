8a6a4998d075c7244947ac12a3998a6a290b6fc5
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-10-26 21:45:16 -0300

    welcome diseñado

--- src/components/Landing/Welcome/Welcome.jsx
+++ src/components/Landing/Welcome/Welcome.jsx
@@ -7,7 +7,21 @@ function Welcome() {
       <div className={style.navbar}></div>
 
       <div className={style.subcontainer}>
-        <div className={style.circulo}></div>
+        <div className={style.circulo}>
+          <div className={style.contenido}>
+            <div className={style.titulo}>
+              <h1>
+                Comienza tu camino como <span>desarrollador</span> junto a
+                nosotros.
+              </h1>
+            </div>
+            <div className={style.boton}>
+              <button>
+                <span>Empieza ya</span>
+              </button>
+            </div>
+          </div>
+        </div>
         <div className={style.imagen}></div>
       </div>
     </div>
