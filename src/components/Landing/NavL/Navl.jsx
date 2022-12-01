21c8c5620983fdda4a6f2ab36ebaaeef51e0d315
gabiinicola <gabrielnicola8@gmail.com>
2022-12-01 12:05:29 -0300

    first commit

--- src/components/Landing/NavL/Navl.jsx
+++ src/components/Landing/NavL/Navl.jsx
@@ -2,35 +2,7 @@ import React from "react";
 import style from "./Navl.module.css";
 function Navl() {
   return (
-    /*    <div>
-      <div className={style.nav}>
-        <ul className={style.ul}>
-          <button className={style.btns}>
-            <span className={style.firstbtn}>
-              Chang<span className={style.e}>e</span>
-            </span>
-          </button>
-
-          <ul className={style.ul2}>
-            <button className={style.btns}>
-              <span className={style.midbtn}>Vision</span>
-            </button>
-            <button className={style.btns}>
-              <span className={style.midbtn}>Que Aprenderás</span>
-            </button>
-            <button className={style.btns}>
-              <span className={style.midbtn}>Como Empezar</span>
-            </button>
-          </ul>
-
-          <ul className={style.ul}>
-            <button className={style.btns}>
-              <span className={style.lastbtn}>Empieza Ya</span>
-            </button>
-          </ul>
-        </ul>
-      </div>
-    </div> */
+   
     <div className={style.container}>
       {/*       a partir de aca vamos armando el orden de como van a ir los items, con classname lo mas explicita posible */}
       <div className={style.logo}>
